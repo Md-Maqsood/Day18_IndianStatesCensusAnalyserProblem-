7ab1f5e950e6f17876f69425380106a9c7dc2fcd
maqsood <maqsoodhamd37@gmail.com>
2020-10-19 12:50:15 +0530

    [Maqsood].add: initial commit

---
+++ src/main/java/com/bridgeLabs/indianStatesCensusAnalyserProgram/StateCensusAnalyser.java
@@ -0,0 +1,12 @@
+package com.bridgeLabs.indianStatesCensusAnalyserProgram;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class StateCensusAnalyser {
+	private static final Logger logger = LogManager.getLogger(StateCensusAnalyser.class);
+
+	public static void main(String[] args) {
+		logger.info("Welcome to Indian States Census Analyser Program");
+	}
+}
---
+++ src/test/java/com/bridgeLabs/indianStatesCensusAnalyserProgram/StateCensusAnalyserTest.java
@@ -0,0 +1,20 @@
+package com.bridgeLabs.indianStatesCensusAnalyserProgram;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class StateCensusAnalyserTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
