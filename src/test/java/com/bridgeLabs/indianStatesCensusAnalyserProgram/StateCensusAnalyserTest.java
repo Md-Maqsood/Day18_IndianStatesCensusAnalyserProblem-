79ceb1f23ecc1063962d10673d18a6490da3c43d
maqsood <maqsoodhamd37@gmail.com>
2020-10-19 19:31:59 +0530

    [Maqsood].update: added test case to check if incorrect csv file returns a custom exception

--- src/test/java/com/bridgeLabs/indianStatesCensusAnalyserProgram/StateCensusAnalyserTest.java
+++ src/test/java/com/bridgeLabs/indianStatesCensusAnalyserProgram/StateCensusAnalyserTest.java
@@ -2,10 +2,11 @@ package com.bridgeLabs.indianStatesCensusAnalyserProgram;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class StateCensusAnalyserTest {
 	public static final String RIGHT_CENSUS_CSV = "src/main/resources/India-Census-Data.csv";
-
+	public static final String WRONG_CENSUS_CSV = "src/main/resources/India-Census-Date.csv";
 	@Test
 	public void givenIndiaCensusDataCsvShouldReturnExactCount() {
 		StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
@@ -16,6 +17,18 @@ public class StateCensusAnalyserTest {
 			e.printStackTrace();
 		}
 
+	}
+	
+	@Test
+	public void givenWrongCsvFileShouldThrowCensusFileProblemException() {
+		try {
+			StateCensusAnalyser stateCensusAnalyser=new StateCensusAnalyser();
+			ExpectedException exceptionRule= ExpectedException.none();
+			exceptionRule.expect(CensusAnalyserException.class);
+			stateCensusAnalyser.loadStateCensusData(WRONG_CENSUS_CSV);
+		}catch(CensusAnalyserException e) {
+			Assert.assertEquals(CensusAnalyserExceptionType.CENSUS_FILE_PROBLEM, e.exceptionType);
+		}
 	}
 
 }
